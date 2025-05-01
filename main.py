eb4696ccf0a39067179bd824f163277c02638772
Austinmc41 <austinmc41@gmail.com>
2025-05-01 15:43:16 -0400

    small cleanup

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 import datetime
 import json
 import os
@@ -10,19 +11,13 @@ load_dotenv()  # take environment variables from .env.
 
 my_secret_key = os.getenv("SECRET_KEY")
 
-def string_cleaner(rouge_text):
-    # "" instead of " " in .join() method
-    return (("".join(rouge_text.strip())
-            .encode('ascii', 'ignore')
-            .decode("utf-8"))
-            .replace("\\",""))
+def clean_job_descs(jobs):
+    for job in job_list:
+        if isinstance(job['description'], str):
+            job['description'] = re.sub(r'\s{2,}', '\n', job['description'])
+            job['description'] = re.sub(r'[^\x00-\x7F]+', '', job['description'])
 
-if __name__ == "__main__":
-    gemini = genai.Client(api_key=my_secret_key)
-    resume_parser = ResumeParser(gemini)
-    parsed_resume = resume_parser.parse_resume('Austin_career_journey.pdf')
-    job_titles = resume_parser.get_job_titles(parsed_resume)
-    job_list = JobScraper.get_jobs(job_titles)
+def convert_dates_iso(jobs):
     for job in job_list:
         date_posted = job["date_posted"]
         if isinstance(date_posted, float):
@@ -30,6 +25,23 @@ if __name__ == "__main__":
         else:
             job["date_posted"] = date_posted.isoformat()
 
+
+if __name__ == "__main__":
+    gemini = genai.Client(api_key=my_secret_key)
+    resume_parser = ResumeParser(gemini)
+    parsed_resume = resume_parser.parse_resume('Austin_career_journey.pdf')
+
+
+    job_titles = resume_parser.get_job_titles(parsed_resume)
+    job_list = JobScraper.get_jobs(job_titles)
+
+    convert_dates_iso(job_list)
+    clean_job_descs(job_list)
+
+
+
+
     jobs = [{"jobs": job_list}]
+
     with open("jobs.json", "w") as f:
         json.dump(jobs, f)
